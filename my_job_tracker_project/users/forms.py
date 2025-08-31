edc0ef82e6ab014c831302000d3b2fbeb7004054
Iman-saidi <summeiyaiman@gmail.com>
2025-08-31 16:48:27 +0300

    adding a new component

--- my_job_tracker_project/users/forms.py
+++ my_job_tracker_project/users/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from django.contrib.forms import
-UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    classMeta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
