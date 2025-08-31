cf152c5f11ab54df74420a8976118d42b3d732ca
Iman-saidi <summeiyaiman@gmail.com>
2025-08-31 17:49:44 +0300

    corrections

--- my_job_tracker_project/users/views.py
+++ my_job_tracker_project/users/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.views import View
 from .forms import CustomUserCreationForm
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+class SignUpView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, "signup.html", {"form": form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # redirect after signup
+        return render(request, "signup.html", {"form": form})
