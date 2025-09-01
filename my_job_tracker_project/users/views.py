b0da487c26f6dc4cee5652b93f61795f29d7b47e
Iman-saidi <summeiyaiman@gmail.com>
2025-09-01 15:51:01 +0300

    corrections

--- my_job_tracker_project/users/views.py
+++ my_job_tracker_project/users/views.py
@@ -1,6 +1,10 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import SignUpForm
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Job Tracker!")
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
