3f2ebbbca6d00ca60fe902a7810c197f1706a8e4
Iman-saidi <summeiyaiman@gmail.com>
2025-09-07 20:53:30 +0300

    more corrections

--- my_job_tracker_project/users/views.py
+++ my_job_tracker_project/users/views.py
@@ -7,10 +7,20 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login') 
     template_name = 'registration/signup.html'
-
+    
 def user_list(request):
-    data = [
+    users = [
         {"id": 1, "username": "iman"},
         {"id": 2, "username": "summeiya"},
     ]
-    return JsonResponse(data, safe=False)    
+    return render(request, "users/user_list.html", {"users": users})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the login page after a successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
